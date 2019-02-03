22a02d9ac6b0dfc4723881851d8f8c278d5e43a6
LaraGarg <lgarg@ec.iitr.ac.in>
2019-02-04 02:05:06 +0530

    created front end

--- brainwaves/settings.py
+++ brainwaves/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sh_app.apps.ShAppConfig', #brainwaves/sh_app/apps.py
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK={'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 20}
+CORS_ORIGIN_ALLOW_ALL = True
